81f73443b9ca5593574afd61fc738613735373cf
Guilherme <guilherme.asalgado@gmail.com>
2024-02-20 19:18:18 -0300

    Adicionando modelo Store ao model

---
+++ app/models/store.rb
@@ -0,0 +1,7 @@
+class Store < ApplicationRecord
+
+  validates :name,
+            presence: true,
+            length: { minimum: 3 }
+
+end
---
+++ spec/models/store_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Store, type: :model do
+
+  describe "validations" do
+    it "should be valid when name is filled" do
+      store = Store.new name: "Greatest store ever!"
+      expect(store.valid?).to eq true
+    end
+
+  end
+
+end
